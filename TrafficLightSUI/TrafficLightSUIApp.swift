c42501fec2df50455a6a45385f6437698bcb9328
AlxGrshn <113042128+AlxGrshn@users.noreply.github.com>
2022-12-10 13:58:53 +0300

    Initial Commit

---
+++ TrafficLightSUI/TrafficLightSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightSUIApp.swift
+//  TrafficLightSUI
+//
+//  Created by Alexander Grishin on 10.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
